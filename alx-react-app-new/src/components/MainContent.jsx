b032e4e816d882e458734723ed2eaa545541434c
James Rotich <rotichjames8@gmail.com>
2025-02-22 23:12:41 +0300

    first commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+  return (
+    <main style={{ backgroundColor: "gray", color: "white" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
