1b370df26a871fd6d04c691168d42a02feab776e
James Rotich <rotichjames8@gmail.com>
2025-02-22 23:23:47 +0300

    first commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <MainContent />
       <Footer />
     </>
