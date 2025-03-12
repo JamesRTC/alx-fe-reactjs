af721c78c7252ebbfda39403da8c192de321d8d3
James Rotich <rotichjames8@gmail.com>
2025-03-12 18:16:01 +0300

    first commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 rounded-lg shadow-lg text-center md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
