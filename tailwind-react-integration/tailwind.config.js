56a9aedeaa2cb0e02bb8b3ceb9c054b179a452e3
James Rotich <rotichjames8@gmail.com>
2025-03-12 18:02:02 +0300

    first commit

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'class' for manual dark mode toggle
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
