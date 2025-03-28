66a5193c00a8424cd9d8faa701d7684928bdf7cc
James Rotich <rotichjames8@gmail.com>
2025-03-28 17:05:26 +0300

    first commit

---
+++ github-user-search/src/components/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Welcome to the GitHub User Search App</h1>;
+};
+
+export default Home;
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,5 @@
+const Search = () => {
+  return <h1>Search for GitHub Users Here</h1>;
+};
+
+export default Search;
